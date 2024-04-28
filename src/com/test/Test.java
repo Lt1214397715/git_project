a7e8fddb38d31ac86b99354a31d51287a668fd67
lt <1214397715@qq.com>
2024-04-28 16:32:11 +0800

    test

---
+++ src/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @author LT
+ * @date 2024/4/28
+ * @project git_project
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
